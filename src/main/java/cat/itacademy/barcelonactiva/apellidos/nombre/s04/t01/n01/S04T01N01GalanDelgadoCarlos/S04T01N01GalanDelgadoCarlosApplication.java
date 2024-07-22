633cbb4ec83301480b82b4cc0690afaa5cfe6487
Carlos Galan <camiloarthuro94@gmail.com>
2024-07-22 12:35:01 +0200

    S4T1 Completed

---
+++ src/main/java/cat/itacademy/barcelonactiva/apellidos/nombre/s04/t01/n01/S04T01N01GalanDelgadoCarlos/S04T01N01GalanDelgadoCarlosApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.apellidos.nombre.s04.t01.n01.S04T01N01GalanDelgadoCarlos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01GalanDelgadoCarlosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01GalanDelgadoCarlosApplication.class, args);
+	}
+
+}
